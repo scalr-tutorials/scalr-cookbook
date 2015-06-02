99a002bf86730b404fcef14ce37988ec479b648d
Thomas Orozco <thomas@orozco.fr>
2015-06-02 13:44:23 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'thomas@scalr.com'
 license          'Apache License v2.0'
 description      'Library to access the Scalr agent in Chef (szradm)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
